a01d5f7bd4110c61614b1e4c6781258ccfc58c9f
Ted Pyne <ted.pyne.4@gmail.com>
2016-05-13 13:29:46 -0400

    Getter

--- src/Brackets.java
+++ src/Brackets.java
@@ -2,7 +2,9 @@
 public class Brackets {
 	private Brackets(){}
 	private static Bracket[] brackets;
+	private static int bSize;
 	public static void setBrackets(int num, int size) {
+		bSize = size;
 		if(brackets!=null) return;
 		brackets = new Bracket[num];
 		for(int i = 0; i < num; i++) brackets[i] = new Bracket(size);
@@ -13,6 +15,9 @@ public class Brackets {
 	}
 	public static int getNum() {
 		return brackets.length;
+	}
+	public static int getSize(){
+		return bSize;
 	}
 	
 }
