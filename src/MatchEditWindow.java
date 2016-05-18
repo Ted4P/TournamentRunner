9c801d94911aa9e93ad4200b6d7eefa9ac810580
BenKelly99 <benjaminkelly@me.com>
2016-05-18 13:33:02 -0400

    Update MatchEditWindow.java

--- src/MatchEditWindow.java
+++ src/MatchEditWindow.java
@@ -18,6 +18,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+import com.sun.glass.ui.View;
+
 public class MatchEditWindow extends JDialog implements ActionListener{
 	private Bracket bracket;
 	private String[] matchInfo;
@@ -27,39 +29,49 @@ public class MatchEditWindow extends JDialog implements ActionListener{
 	private JPanel comboBox, options;
 	private GridBagConstraints con;
 	private TournamentView view;
-	public MatchEditWindow(TournamentView view, Bracket bracket, String[] matchInfo){
+	private int match;
+	public MatchEditWindow(TournamentView view, Bracket bracket, String[] matchInfo, int match){
 		super(view, "Match Info");
 		this.view = view;
 		this.bracket = bracket;
 		this.matchInfo = matchInfo;
+		this.match = match;
 		name1 = new JTextField(matchInfo[0]);
 		name1.setColumns(10);
+		name1.setHorizontalAlignment(JTextField.CENTER);
 		school1 = new JTextField(matchInfo[1]);
 		school1.setColumns(10);
+		school1.setHorizontalAlignment(JTextField.CENTER);
 		name2 = new JTextField(matchInfo[2]);
 		name2.setColumns(10);
+		name2.setHorizontalAlignment(JTextField.CENTER);
 		school2 = new JTextField(matchInfo[3]);
 		school2.setColumns(10);
+		school2.setHorizontalAlignment(JTextField.CENTER);
 		notes = new JTextField(matchInfo[6]);
-		name1.setEditable(false);
-		name2.setEditable(false);
-		school1.setEditable(false);
-		school2.setEditable(false);
-		notes.setEditable(false);
-		winnerEdit = new JComboBox(new String[]{"Unplayed",name1.getText(),name2.getText()});
+		String currWinner = matchInfo[3] + "(" + matchInfo[4] + ")";
+		winnerEdit = new JComboBox(new String[]{"Unplayed",name1.getText() + "(" + school1.getText() + ")",name2.getText() + "(" + school2.getText() + ")"});
+		if(winnerEdit.getItemAt(1).equals(currWinner))
+			winnerEdit.setSelectedIndex(1);
+		else if(winnerEdit.getItemAt(2).equals(currWinner))
+			winnerEdit.setSelectedIndex(2);
+		else
+			winnerEdit.setSelectedIndex(0);
+		winnerEdit.setEnabled(false);
+		JPanel labels = new JPanel();
+		labels.setLayout(new GridBagLayout());
+		labels.add(new JLabel("Player A          vs          Player B"));
 		JPanel comps = new JPanel();
 		comps.setLayout(new GridBagLayout());
 		GridBagConstraints con = new GridBagConstraints();
-		con.gridheight = 10;
+		con.gridheight = 5;
 		con.gridwidth = this.getWidth();
 		con.fill = GridBagConstraints.BOTH;
 		comps.add(name1);
-		comps.add(new JLabel(" vs "));
 		comps.add(name2);
 		JPanel schools = new JPanel();
 		schools.setLayout(new GridBagLayout());
 		schools.add(school1);
-		schools.add(new JLabel("  "));
 		schools.add(school2);
 		options = new JPanel();
 		options.setLayout(new GridBagLayout());
@@ -76,41 +88,32 @@ public class MatchEditWindow extends JDialog implements ActionListener{
 		options.add(close);
 		comboBox = new JPanel();
 		comboBox.setLayout(new GridBagLayout());
-		setLayout(new GridLayout(5,1));
+		comboBox.add(new JLabel("Winner: " + winnerEdit.getSelectedItem()));
+		setLayout(new GridLayout(6,1));
 		add(comboBox);
+		add(labels);
 		add(comps);
 		add(schools);
 		add(notes);
 		add(options);
 		DocumentListener docListen = new DocumentListener(){
 			public void changedUpdate(DocumentEvent e){
-				enableButtons();
+				updateWhileEditing();
 			}
 			public void removeUpdate(DocumentEvent e){
-				enableButtons();
+				updateWhileEditing();
 			}
 			public void insertUpdate(DocumentEvent e){
-				enableButtons();
-			}
-			private void enableButtons(){
-				int index = winnerEdit.getSelectedIndex();
-				winnerEdit.removeAllItems();
-				winnerEdit.addItem("Unplayed");
-				if(name1.equals(name2) || name1.getText().equals("") || name1.getText().equals("TBD") || name1.getText().equals(Match.DEFAULT_WINNER_NAME) || name2.getText().equals("") || name2.getText().equals("TBD") || name2.getText().equals(Match.DEFAULT_WINNER_NAME)){
-					save.setEnabled(false);
-			}
-				else{
-					winnerEdit.addItem(name1.getText());
-					winnerEdit.addItem(name2.getText());
-					winnerEdit.setSelectedIndex(index);
-					save.setEnabled(true);
-					repaint();
-				}
+				updateWhileEditing();
 			}
 		};
 		name1.getDocument().addDocumentListener(docListen);
 		name2.getDocument().addDocumentListener(docListen);
+		school1.getDocument().addDocumentListener(docListen);
+		school2.getDocument().addDocumentListener(docListen);
+		notes.getDocument().addDocumentListener(docListen);
 		name1.setText(name1.getText());
+		viewMode();
 		Toolkit tlkt = Toolkit.getDefaultToolkit();
 		setModal(true);
 		setResizable(true);
@@ -119,7 +122,7 @@ public class MatchEditWindow extends JDialog implements ActionListener{
 		setAlwaysOnTop(true);
 		setVisible(true);
 	}
-	
+
 	private void viewMode(){
 		Border emptyBorder = BorderFactory.createEmptyBorder();
 		name1.setEditable(false);
@@ -132,14 +135,15 @@ public class MatchEditWindow extends JDialog implements ActionListener{
 		school2.setBorder(emptyBorder);
 		notes.setEditable(false);
 		notes.setBorder(emptyBorder);
-		comboBox.removeAll();
-		comboBox.add(new JLabel("Winner: " + winnerEdit.getSelectedItem()));
 		options.removeAll();
 		options.add(edit);
 		options.add(close);
+		comboBox.removeAll();
+		comboBox.add(new JLabel("Winner: " + winnerEdit.getSelectedItem()));
+		comboBox.repaint();
 		repaint();
 	}
-	
+
 	private void editMode(){
 		if(Integer.parseInt(matchInfo[7]) > Brackets.getSize()/2){
 			Border drawBorder = BorderFactory.createLineBorder(Color.black, 1);
@@ -154,28 +158,57 @@ public class MatchEditWindow extends JDialog implements ActionListener{
 			notes.setBorder(drawBorder);
 		}
 		notes.setEditable(true);
-		comboBox.removeAll();
-		comboBox.add(winnerEdit);
+		winnerEdit.setEnabled(true);
 		options.removeAll();
 		options.add(save);
 		options.add(cancel);
+		comboBox.removeAll();
+		comboBox.add(new JLabel("Winner: "));
+		comboBox.add(winnerEdit);
+		updateWhileEditing();
 		repaint();
 	}
 	
+	public void updateWhileEditing(){
+		int index = winnerEdit.getSelectedIndex();
+		winnerEdit.removeAllItems();
+		winnerEdit.addItem("Unplayed");
+		if(name1.equals(name2) ||
+				name1.getText().equals("") || name1.getText().equals("TBD") || name1.getText().equals(Match.DEFAULT_WINNER_NAME) ||
+				name2.getText().equals("") || name2.getText().equals("TBD") || name2.getText().equals(Match.DEFAULT_WINNER_NAME) ||
+				school1.getText().equals("") || school1.getText().equals("TBD") || school1.getText().equals(Match.DEFAULT_WINNER_SCHOOL) ||
+				school2.getText().equals("") || school2.getText().equals("TBD") || school2.getText().equals(Match.DEFAULT_WINNER_SCHOOL) ||
+				notes.getText().equals(""))
+			save.setEnabled(false);
+		else{
+			winnerEdit.addItem(name1.getText()+"("+school1.getText()+")");
+			winnerEdit.addItem(name2.getText()+"("+school2.getText()+")");
+			winnerEdit.setSelectedIndex(index);
+			save.setEnabled(true);
+			repaint();
+		}
+		comboBox.repaint();
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if(arg0.getSource() == save){
+			Person p1 = new Person(name1.getText(),school1.getText());
+			Person p2 = new Person(name2.getText(), school2.getText());
 			Person toBePromoted;
-			if(winnerEdit.getSelectedItem().equals(name1.getText()))
-				toBePromoted = new Person(name1.getText(), school1.getText());
-			else if(winnerEdit.getSelectedItem().equals(name2.getText()))
-				toBePromoted = new Person(name2.getText(), school2.getText());
+			if(winnerEdit.getSelectedItem().equals(name1.getText() + "(" + school1.getText() + ")"))
+				toBePromoted = p1;
+			else if(winnerEdit.getSelectedItem().equals(name2.getText() + "(" + school2.getText() + ")"))
+				toBePromoted = p2;
 			else
 				toBePromoted = new Person(Match.DEFAULT_WINNER_NAME,Match.DEFAULT_WINNER_SCHOOL);
-			view.promotePerson(toBePromoted,Brackets.indexOf(bracket),notes.getText());
+			winnerEdit.setEnabled(false);
+			view.setMatch(p1,p2,toBePromoted,notes.getText(),Brackets.indexOf(bracket),match);
+			view.promotePerson(toBePromoted,Brackets.indexOf(bracket),notes.getText(),match);
 			viewMode();
 		}
 		if(arg0.getSource() == cancel){
+			winnerEdit.setEnabled(false);
 			viewMode();
 		}
 		if(arg0.getSource() == close){
