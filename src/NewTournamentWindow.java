66f9fc85ccaebbbec54bc660f8de8db2de87ea50
Ted Pyne <ted.pyne.4@gmail.com>
2016-05-20 16:43:35 -0400

    Start of method separation

---
+++ src/NewTournamentWindow.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDialog;
+
+
+public class NewTournamentWindow extends JDialog implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
