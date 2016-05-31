1cfff90d05de4f51628a0855aef72b9141db1fa2
BenKelly99 <benjaminkelly@me.com>
2016-05-30 21:24:16 -0400

    Update MatchButton.java

--- src/windows/MatchButton.java
+++ src/windows/MatchButton.java
@@ -1,13 +1,15 @@
 package windows;
 
 import javax.swing.JButton;
-
+//Represents a button for a match edit on the panel
 public class MatchButton extends JButton{
 	private final int match;
+	//Makes a new button with label 'Edit' and sets match to given match num
 	public MatchButton(int match){
 		super("Edit");
 		this.match = match;
 	}
+	//returns which match this button represents
 	public int getMatch(){
 		return match;
 	}
